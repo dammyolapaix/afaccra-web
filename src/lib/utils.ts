6a332b5361997c26b547c7a684cff78ffb3a77ea
Dammy Ola <dammyolapaix@gmail.com>
2024-07-02 16:46:11 +0000

    Added formatToMoney utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatToMoney = (amount: string | number): string =>
+  `GHS ${(Number(amount) / 100)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
