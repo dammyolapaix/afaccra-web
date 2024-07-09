f3a67a076db05b566c14b4048129a0402a8f0cda
Dammy Ola <dammyolapaix@gmail.com>
2024-07-09 11:49:42 +0000

    Modify the useSearchParamsQuery hook to get params

--- src/hooks/use-search-params-query.tsx
+++ src/hooks/use-search-params-query.tsx
@@ -1,15 +1,19 @@
 'use client'
 
 import { useSearchParams, usePathname, useRouter } from 'next/navigation'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function useSearchParamsQuery({
   query,
   value,
+  action,
 }: {
   query: string
   value?: string
+  action?: 'get'
 }) {
+  const [getParams, setGetParams] = useState<string | undefined | null>(null)
+
   const searchParams = useSearchParams()
   const params = new URLSearchParams(searchParams)
 
@@ -17,13 +21,18 @@ export default function useSearchParamsQuery({
   const { replace } = useRouter()
 
   useEffect(() => {
-    if (value) {
-      params.set(query, value)
+    if (action === 'get') {
+      setGetParams(params.get(query))
     } else {
-      params.delete(query)
+      if (value) {
+        params.set(query, value)
+      } else {
+        params.delete(query)
+      }
+      replace(`${pathname}?${params.toString()}`)
     }
-    replace(`${pathname}?${params.toString()}`)
-  }, [query, value])
+  }, [query, value, action])
 
+  if (action === 'get') return getParams
   return
 }
